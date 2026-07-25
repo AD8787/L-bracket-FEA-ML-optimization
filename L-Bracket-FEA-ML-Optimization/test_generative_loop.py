26f10773de0f675b8cc97aecce070f35dfe5b487
Aarush Dhawan <75181003+AD8787@users.noreply.github.com>
2026-07-24 17:41:39 -0700

    Modify ANN model structure and enhance comments
    Updated the neural network model architecture and added comments for clarity.

--- L-Bracket-FEA-ML-Optimization/test_generative_loop.py
+++ L-Bracket-FEA-ML-Optimization/test_generative_loop.py
@@ -11,11 +11,11 @@ target = bracket_data["Min_Safety_Factor"]
 data_scaler = StandardScaler()
 scaled_features = data_scaler.fit_transform(features)
 
-ann_model = MLPRegressor(hidden_layer_sizes = (10, 10), activation = "relu", max_iter = 2000, random_state = 42)
+ann_model = MLPRegressor(hidden_layer_sizes = (75, 75), activation = "relu", max_iter = 2000, random_state = 42)
 ann_model.fit(scaled_features, target)
 
-abs_min_sf = 2.0
-target_sf = 4.0
+abs_min_sf = 2.0 #absolute minimum safety factor considered before part is rendered unsuable
+target_sf = 4.0 #the safety factor each part should target to be at
 num_candidates = 10000
 np.random.seed(42)
 
